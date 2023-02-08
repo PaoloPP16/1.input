5b8a380e6da1c6fb0ea1fe2520399e9c17d7fc73
PaoloPP16 <pahama2018@gmail.com>
2023-02-08 09:10:14 -0500

    inputCommit

---
+++ main.js
@@ -0,0 +1,10 @@
+let arregloButton = []
+let resultado =document.getElementById("resultado")
+document.getElementById("buttonId").addEventListener("click",() =>{
+
+    let buttonVariable = document.querySelector("#imputId").value;
+    resultado.innerHTML+=<div>${buttonVariable}</div>
+
+});
+
+
